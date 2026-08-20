0f2db5a02507f76ef8a3b5819588349ee6f36adc
S H Mahammed Saleeth <selith514@gmail.com>
2026-08-20 13:56:19 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello, World!' });
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
